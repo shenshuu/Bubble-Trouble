70128ed09b0fa89f109f52ec304fee8d382d996a
Michael Shen <michaelshen85@gmail.com>
2022-07-27 00:04:34 -0400

    Add Game class

---
+++ src/scripts/game.js
@@ -0,0 +1,8 @@
+export default class Game {
+    constructor(player, enemies, canvas, ctx) {
+        this.player = player;
+        this.enemies = enemies;
+        this.canvas = canvas;
+        this.ctx = ctx;
+    }
+}
